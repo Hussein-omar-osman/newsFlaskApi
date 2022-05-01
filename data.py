182c2223e1dc58cf5b9c86f8e06ad1b6e7f2ab5f
Hussein Omar <husseinomar6190@gmail.com>
2022-05-01 16:26:14 +0300

    creating a data.py file to store the data for the home page

---
+++ data.py
@@ -0,0 +1,163 @@
+posts = [
+        {
+            "source": {
+                "id": "cnn",
+                "name": "CNN"
+            },
+            "author": "Nectar Gan and CNN's Beijing Bureau",
+            "title": "Beijing shuts Universal Studios, bans restaurant dining in major escalation of Covid restrictions - CNN",
+            "description": "Beijing has banned all restaurant dining, shut down Universal Studios and ordered residents to provide proof of a negative Covid test to enter public venues in a major escalation of restrictions as a five-day holiday gets underway.",
+            "url": "https://www.cnn.com/2022/05/01/china/beijing-covid-labor-day-holiday-intl-hnk/index.html",
+            "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/220430214012-beijing-covid-050122-restricted-super-tease.jpg",
+            "publishedAt": "2022-05-01T05:24:00Z",
+            "content": None
+        },
+        {
+            "source": {
+                "id": None,
+                "name": "WAPT Jackson"
+            },
+            "author": "Gracyn Gordon",
+            "title": "One dead, multiple shot at Mudbugs Festival at state fairgrounds - WAPT Jackson",
+            "description": "One dead, multiple injured, according to police",
+            "url": "https://www.wapt.com/article/one-dead-multiple-shot-at-mudbugs-festival-at-state-fairgrounds/39869754",
+            "urlToImage": "https://kubrick.htvapps.com/htv-prod/ibmig/cms/image/wapt/13381576-generic-red-blue-police-lights-nighttime-jpg.jpg?crop=1.00xw:0.752xh;0,0&resize=1200:*",
+            "publishedAt": "2022-05-01T04:47:00Z",
+            "content": "JACKSON, Miss. —Hinds County Sheriff Tyree Jones says multiple people were shot at the state fairgrounds Saturday night during the Mudbugs Festival.\r\nOne person is dead and several injured. Police be… [+313 chars]"
+        },
+        {
+            "source": {
+                "id": None,
+                "name": "ESPN"
+            },
+            "author": "Mel Kiper Jr.",
+            "title": "NFL draft grades - Mel Kiper Jr. picks steals, sleepers and favorite 2022 classes from all 32 teams - ESPN",
+            "description": "The Texans got a few foundational prospects. The Lions got an in-state star. And the Titans might have gotten the steal of the draft.",
+            "url": "https://www.espn.com/nfl/insider/draft2022/insider/story/_/id/33827196/nfl-draft-grades-mel-kiper-jr-picks-steals-sleepers-favorite-2022-classes-all-32-teams",
+            "urlToImage": "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F0429%2Fnfl_mel%2Dkipers%2Ddraft%2Dgrades_16x9.jpg",
+            "publishedAt": "2022-05-01T04:34:07Z",
+            "content": "This was my 39th year doing the NFL draft for ESPN, and I can't remember a trading frenzy quite like the one we saw in the middle of Round 1 on Thursday. There were picks and players flying off the b… [+73408 chars]"
+        },
+        {
+            "source": {
+                "id": "politico",
+                "name": "Politico"
+            },
+            "author": None,
+            "title": "Trevor Noah's top White House Correspondents Dinner moments - POLITICO - POLITICO",
+            "description": "The headliner quipped that it was his “great honor” to speak at the “nation’s most distinguished super-spreader event.”",
+            "url": "https://www.politico.com/news/2022/05/01/trevor-noah-white-house-correspondents-highlights-best-moments-00029073",
+            "urlToImage": "https://static.politico.com/6e/24/de8fe01a4c6a997c06607cc564cd/biden-correspondents-dinner-02722.jpg",
+            "publishedAt": "2022-05-01T04:25:02Z",
+            "content": "Trevor Noah, host of Comedy Central's \"The Daily Show,\" speaks at the annual White House Correspondents' Association dinner, Saturday, April 30, 2022, in Washington. | Patrick Semansky/AP Photo\r\nDail… [+510 chars]"
+        },      
+]
+
+sports = [
+        {
+            "source": {
+                "id": None,
+                "name": "The Guardian"
+            },
+            "author": "Kim Willsher",
+            "title": "Far-right politician booted out of Zinedine Zidane’s football club",
+            "description": "Eric Zemmour ejected after club says it does not want its image associated with French presidential candidateIn the unexpected clash between an anti-immigration French politician and a sporting hero whose parents emigrated to France, the final result was clea…",
+            "url": "https://amp.theguardian.com/world/2022/apr/03/french-far-right-politician-eric-zemmour-booted-out-of-zinedine-zidane-football-club",
+            "urlToImage": "https://i.guim.co.uk/img/media/12ca57a42caaf563e3eab63227ee224b69640402/189_225_3290_1975/master/3290.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=db39624f669aa4610c364017bedb6317",
+            "publishedAt": "2022-04-03T13:16:58Z",
+            "content": "In the unexpected clash between an anti-immigration French politician and a sporting hero whose parents emigrated to France, the final result was clear: Zemmour 0 Zidane 1.\r\nA week from the first rou… [+2934 chars]"
+        },
+        {
+            "source": {
+                "id": None,
+                "name": "New York Times"
+            },
+            "author": "Amelia Nierenberg",
+            "title": "Gov. Andrew Beshear of Kentucky Vetoes Transgender Sports Bill",
+            "description": "The State Legislature, which is dominated by Republicans, may override the veto. Twelve states have enacted legislation barring young transgender athletes from participating in girls’ sports.",
+            "url": "https://www.nytimes.com/2022/04/06/us/kentucky-governor-transgender.html",
+            "urlToImage": "https://static01.nyt.com/images/2022/04/06/us/06xp-Kentucky-trans/merlin_205104561_d15104ee-56f5-4b32-9035-051a61c9cf6b-facebookJumbo.jpg",
+            "publishedAt": "2022-04-07T03:33:13Z",
+            "content": "The Fairness Campaign, a state L.G.B.T.Q. rights organization, has said it was aware of only one openly transgender student-athlete in Kentucky. She started her schools field hockey team, said the gr… [+1398 chars]"
+        },
+        {
+            "source": {
+                "id": "bbc-news",
+                "name": "BBC News"
+            },
+            "author": None,
+            "title": "Women's World Cup final: Australia outclass England to take title",
+            "description": "Australia underline their status as the premier international sports team on the planet by outclassing England to win the Women's World Cup.",
+            "url": "https://www.bbc.co.uk/sport/cricket/60971139",
+            "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/14383/production/_123991828_aussies.png",
+            "publishedAt": "2022-04-03T08:05:08Z",
+            "content": "<table><tr><th>Women's World Cup final, Christchurch</th></tr>\r\n<tr><td>Australia 356-5 (50 overs): Healy 170, Haynes 68, Shrubsole 3-46</td></tr><tr><td>England 285 (43.4 overs): Sciver 148*, Jonass… [+4749 chars]"
+        },
+        {
+            "source": {
+                "id": None,
+                "name": "The Guardian"
+            },
+            "author": "Dumitru Garcaliuc in Desna and Emanuel Rosu",
+            "title": "Football in Ukraine: ‘Where will the kids train now?’",
+            "description": "Chernihiv has been destroyed by Russian forces, including the football stadium, but the local population is refusing to give upChernihiv is the largest Ukrainian city to have been freed from Russian occupation. Its pre-war population was estimated at around 2…",
+            "url": "https://amp.theguardian.com/football/2022/apr/22/football-in-ukraine-where-will-the-kids-train-now",
+            "urlToImage": "https://i.guim.co.uk/img/media/b03f90754e1edcc5cea4127d916a50f774fe6072/0_133_5886_3532/master/5886.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=2b1805767c5127fe688378da289da4cc",
+            "publishedAt": "2022-04-22T19:00:32Z",
+            "content": "Chernihiv is the largest Ukrainian city to have been freed from Russian occupation. Its pre-war population was estimated at around 280,000. After being under siege for more than a month only a third … [+5709 chars]"
+        }
+]
+crypto = [
+          {
+            "source": {
+                "id": "engadget",
+                "name": "Engadget"
+            },
+            "author": "Mariella Moon",
+            "title": "Wikipedia editors vote to block cryptocurrency donations",
+            "description": "Wikipedia editors have voted in favor of dropping cryptocurrency from the Wikimedia Foundation's donation options. As Ars Technica reports, an editor for the online encyclopedia called GorillaWarfare wrote a proposal for the foundation to stop accepting crypt…",
+            "url": "https://www.engadget.com/wikipedia-editors-vote-to-block-cryptocurrency-donations-113549175.html",
+            "urlToImage": "https://s.yimg.com/os/creatr-uploaded-images/2021-07/9f595ce0-de17-11eb-bef2-e1b1456d84ae",
+            "publishedAt": "2022-04-14T11:35:49Z",
+            "content": "Wikipedia editors have voted in favor of dropping cryptocurrency from the Wikimedia Foundation's donation options. As Ars Technica reports, an editor for the online encyclopedia called GorillaWarfare… [+1577 chars]"
+        },
+        {
+            "source": {
+                "id": "the-verge",
+                "name": "The Verge"
+            },
+            "author": "Emma Roth",
+            "title": "Telegram now lets users send cryptocurrency through TON blockchain spinoff",
+            "description": "Telegram is now letting users send cryptocurrency payments through The Open Network (TON), the blockchain built off of Telegram’s abandoned crypto efforts.",
+            "url": "https://www.theverge.com/2022/4/30/23050263/telegram-lets-users-send-cryptocurrency-chats-toncoin-ton",
+            "urlToImage": "https://cdn.vox-cdn.com/thumbor/UdhpgI5DTqayp4dfCSt4IyIxBMA=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/10668743/acastro_180417_1777_telegram_0002.jpg",
+            "publishedAt": "2022-04-30T15:56:43Z",
+            "content": "The Open Network (TON) was built off of Telegrams deserted cryptocurrency project\r\nIllustration by Alex Castro / The Verge\r\nTelegram now lets users send Toncoin, the cryptocurrency built off of Teleg… [+2008 chars]"
+        },
+        {
+            "source": {
+                "id": "the-verge",
+                "name": "The Verge"
+            },
+            "author": "Emma Roth",
+            "title": "Germany shuts down servers for Russian darknet marketplace Hydra",
+            "description": "German authorities have shut down and seized the infrastructure of servers associated with the Russian illegal darknet marketplace Hydra.",
+            "url": "https://www.theverge.com/2022/4/5/23011377/germany-servers-russian-darknet-site-hydra-bitcoin",
+            "urlToImage": "https://cdn.vox-cdn.com/thumbor/p230_bbmfO1qQ5ZuapX9JJbHPiY=/0x146:2040x1214/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/11556959/acastro_170621_1777_0001_fin.jpg",
+            "publishedAt": "2022-04-05T19:24:01Z",
+            "content": "German authorities have seized $25.2 million USD in Bitcoin\r\nGerman authorities shut down the server infrastructure for the Russian darknet marketplace Hydra, seizing 23 million (~$25.2 million USD) … [+2344 chars]"
+        },
+        {
+            "source": {
+                "id": "wired",
+                "name": "Wired"
+            },
+            "author": "Andy Greenberg",
+            "title": "Shutdown of Russia's Hydra Market Disrupts a Crypto-Crime ATM",
+            "description": "More than just a market for illegal drugs, the dark web site allowed criminals to launder or cash out hundreds of millions in stolen cryptocurrencies.",
+            "url": "https://www.wired.com/story/hydra-market-shutdown/",
+            "urlToImage": "https://media.wired.com/photos/624c86b5c736857145d140f2/191:100/w_1280,c_limit/Russia-DarkWeb-Security-1303387824.jpg",
+            "publishedAt": "2022-04-05T19:27:56Z",
+            "content": "On the dark web, the takedown of yet another cryptocurrency-based black market for drugs has become almost a semiannual routine, with plenty of competitors ready to fill the shoes of any market that … [+3524 chars]"
+        }
+]
