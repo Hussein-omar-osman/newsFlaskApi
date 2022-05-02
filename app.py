aac48f67219e96dc5e34b485953ed4d51932a451
Hussein Omar <husseinomar6190@gmail.com>
2022-05-02 16:52:15 +0300

    creating a search template to display the html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def index():
  return render_template('index.html', 
                         sports=dt.sports, crypto=dt.crypto, 
@@ -12,8 +13,10 @@ def index():
 
 @app.route("/searchResult", methods=['POST'])
 def searchResult():
- name = request.form['search']
- return render_template('/searchResult.html', name=name)
+ name = request.form['search'].title()
+ api_key = '17e0788966b6404ab6e3a6997bfbacf2'
+ res = requests.get(f'https://newsapi.org/v2/everything?q={name}&apiKey={api_key}').json()
+ return render_template('/searchResult.html', name=name, datas=res)
  
 if __name__ == "__main__":
  app.run(debug=True)
