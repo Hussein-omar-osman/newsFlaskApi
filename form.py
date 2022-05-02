47ba13b213e24a67e62a3564d6cb8ccfa3797c5b
Hussein Omar <husseinomar6190@gmail.com>
2022-05-02 17:14:05 +0300

    adding readme

--- form.py
+++ form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import Length, Email, DataRequired, EqualTo
+from wtforms import SubmitField, StringField
+from wtforms.validators import Length, DataRequired
 
 class SearchForm(FlaskForm):
     SearchTerm = StringField('SearchTerm', validators=[DataRequired(), Length(min=2, max=20)])
