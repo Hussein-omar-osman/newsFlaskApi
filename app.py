316592cd0ea43775328649babb7ce41225d88441
Hussein Omar <husseinomar6190@gmail.com>
2022-05-02 14:59:48 +0300

    adding requests module to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import requests
 import data as dt
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ def index():
                         sports=dt.sports, crypto=dt.crypto, 
                         covid=dt.covid, business=dt.business)
 
-
+@app.route("/searchResult", methods=['POST'])
+def searchResult():
+ name = request.form['search']
+ return render_template('/searchResult.html', name=name)
+ 
 if __name__ == "__main__":
  app.run(debug=True)
